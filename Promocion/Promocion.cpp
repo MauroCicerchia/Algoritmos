08deb842f642b514b9062e49b9b2c264e009ce78
MauroCicerchia <maurocicerchia98@gmail.com>
2017-04-28 13:06:02 -0300

    Promocion

--- Promocion/Promocion.cpp
+++ Promocion/Promocion.cpp
@@ -25,12 +25,33 @@ int main()
 	cin >> notas[3];
 	
 	if(promociona(notas, nombre))
-		cout << "hola";
+		cout << "El alumno " << nombre << " promociono la materia.";
 	else
-		cout << "chau";
+		cout << "El alumno " << nombre << " no promociono la materia.";
 }
 
 bool promociona(int notas[], string nombre)
 {
-	
+	if(notas[0] >= 8 && notas[2] >=8)
+	{
+		return true;
+	} else {
+		if(notas[0] >= 8)
+		{
+			if(notas[3] >= 8)
+				return true;
+			else
+				return false;
+		} else {
+			if(notas[2] >= 8)
+			{
+				if(notas[1] >= 8)
+					return true;
+				else
+					return false;
+			} else {
+				return false;
+			} 
+		}
+	}	
 }
